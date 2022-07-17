37b5b2298cf834cd4c59e41079a57595e749ecce
asletkin <asletkin@mail.ru>
2022-07-17 12:21:23 +0300

    GB_C#_2

--- Program.cs
+++ Program.cs
@@ -1,2 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿////Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+////и на выходе показывает вторую цифру этого числа.
+//
+//int number = new Random().Next(-999, 1000);
+//
+//if (number >= 100 && number < 1000)
+//{
+//    number = number / 10 % 10;
+//    Console.WriteLine(number);
+//}
+//
+//else if (number <= -100 && number > -1000)
+//{
+//    string numberStr = Convert.ToString(number);
+//    Console.WriteLine(numberStr[3]);
+//}
+//
+//else
+//{
+//    Console.WriteLine($"Число {number} не трехзначное!");
+//}
+////---------------------------------------------------------------------------
+////Задача 13: Напишите программу, которая выводит третью цифру заданного числа
+////или сообщает, что третьей цифры нет.
+//
+//int number = new Random().Next();
+//
+//if (number >= 100)
+//{
+//    string numberStr = Convert.ToString(number);
+//    Console.WriteLine(numberStr[2]);
+//}
+//
+//else if (number <= -100)
+//{
+//    string numberStr = Convert.ToString(number);
+//    Console.WriteLine(numberStr[3]);
+//}
+//
+//else
+//{
+//    Console.WriteLine($"В числе {number} нет 3й цифры!");
+//}
+////---------------------------------------------------------------------------
+////Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+////и проверяет, является ли этот день выходным.
+//
+//int weekDay = new Random().Next(1, 8);
+//
+//if (weekDay == 6 || weekDay == 7)
+//{
+//    Console.WriteLine("Выходной");
+//}
+//else
+//{
+//    Console.WriteLine("Будней");
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
